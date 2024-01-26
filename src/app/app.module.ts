33ceaf2dde92ea683c027383e93f3076828d890b
alinatelychko <adelin9999@gmail.com>
2024-01-26 14:16:44 +0100

    6.3 update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app.routes';
 import { AppComponent } from './app.component';
 import { MatInputModule } from '@angular/material/input';
